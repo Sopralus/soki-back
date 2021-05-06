06ce6c84e00e9b08b913e3251713959ddf8c4dca
Manon <manon.bonnet@hetic.net>
2021-05-06 12:41:49 +0200

    [js] fusions

--- index.js
+++ index.js
@@ -4,13 +4,59 @@
  * Module dependencies.
  */
 
-var app = require('./app');
-var debug = require('debug')('soki-back:server');
-var http = require('http');
+ var createError = require('http-errors');
+ var express = require('express');
+ var http = require('http');
+ var path = require('path');
+ var cookieParser = require('cookie-parser');
+ var logger = require('morgan');
+ var cors = require('cors');
+ var mongoose = require('mongoose');
+ 
+ require('./models/User');
+ 
+ mongoose.connect('mongodb://localhost/soki');
+ 
+ var indexRouter = require('./routes/index');
+ var usersRouter = require('./routes/users');
+ 
+ var app = express();
+ 
+ // view engine setup
+ app.set('views', path.join(__dirname, 'views'));
+ app.set('view engine', 'jade');
+ 
+ 
+ app.use(cors());
+ app.use(logger('dev'));
+ app.use(express.json());
+ app.use(express.urlencoded({ extended: false }));
+ app.use(cookieParser());
+ app.use(express.static(path.join(__dirname, 'public')));
+ 
+ app.use('/', indexRouter);
+ app.use('/users', usersRouter);
+ 
+ // catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   next(createError(404));
+ });
+ 
+ // error handler
+ app.use(function(err, req, res, next) {
+   // set locals, only providing error in development
+   res.locals.message = err.message;
+   res.locals.error = req.app.get('env') === 'development' ? err : {};
+ 
+   // render the error page
+   res.status(err.status || 500);
+   res.render('error');
+ });
 
-const express = require('express');
-const { Server } = require("socket.io");
-const io = new Server(server);
+var debug = require('debug')('soki-back:server');
+
+
+
 
  /**
   * Get port from environment and store in Express.
@@ -92,6 +138,14 @@ const io = new Server(server);
      : 'port ' + addr.port;
    debug('Listening on ' + bind);
  }
+
+var io = require("socket.io")(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
 
 app.get('/', (req, res) => {
   res.sendFile('../soki-front/rtmp/index.html');
